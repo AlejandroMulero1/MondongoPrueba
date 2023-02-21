91c76494a26c01dfaab9f537e63f873bc8cfebe3
AlejandroMulero1 <alejandro.mulero@iesnervion.es>
2023-02-21 14:18:39 +0100

    hacer casa/viernes

--- src/main/java/org/example/CRUD.java
+++ src/main/java/org/example/CRUD.java
@@ -3,8 +3,10 @@ package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.result.InsertOneResult;
 import org.bson.Document;
 import org.example.Entidades.Personas;
 
@@ -13,10 +15,27 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class CRUD {
-    public static void Create(MongoCollection<Document> collection){}
+    /**
+     * Método que inserta en una colección pasada por parámetro un documento también pasado por parámetro
+     * @param collection : colección en la cual se quiere insertar
+     * @param obj : registro a insertar
+     */
+    public static void Create(MongoCollection<Document> collection, Document obj){
+        try {
+            //Inserto a través del metodo insertOne de collection
+            InsertOneResult result = collection.insertOne(obj);
 
+            //Muestro la id del registro insertado
+            System.out.println("Usuario insertado con id: " + result.getInsertedId());
+        } catch (MongoException me) {
+            System.err.println("Ha ocurrido un error: " + me);
+        }
+    }
 
-
+    /**
+     * Método que lee todos los registros de una colección pasada por parámetro mostrandolos por pantalla
+     * @param collection
+     */
     public static void Read(MongoCollection<Document> collection){
         //Inicializo un cursor con el que recorrer la coleccion
         try (MongoCursor< Document > cur = collection.find().iterator()) {
@@ -51,6 +70,6 @@ public class CRUD {
         }
     }
 
-    public static void Update(MongoCollection<Document> collection,Serializable key, String registroActualizado){}
+    public static void Update(MongoCollection<Document> collection,Serializable key, Document registroActualizado){}
     public static void Delete(MongoCollection<Document> collection,Serializable key){}
 }
